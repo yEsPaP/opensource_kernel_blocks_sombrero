0af8dae9ea4a4cc29cd4607412fc297a6e6c8f99
Minstrel Chiu <minstrelsy@gmail.com>
2017-06-14 18:17:04 +0800

    Add scripts, info, tools for building the kernel
    Also add a toolchain

--- kernel-build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-export ARCH=arm
-export ARCH_MTK_PLATFORM=mt6580
-export MTK_PLATFORM=mt6580
-export CROSS_COMPILE=arm-linux-androideabi-
-make sf6580_weg_l_defconfig
-make $1
-#make zImage $1
